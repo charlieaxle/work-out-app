70f2a580d360ba9741495a96f91247fd7d4849b7
mdillon <mdillon@capricorn.hsd1.ma.comcast.net>
2017-01-11 20:51:13 -0500

    Enhanced addRoutine functionality

--- public/js/2-workupp.js
+++ public/js/2-workupp.js
@@ -6,8 +6,6 @@ function createUser(vuserName, vfirstName,vlastName){
 		dataType: 'HTTP',
 		data: {userName: vuserName, firstName: vfirstName, lastName: vlastName },
 		url:'../users',
-    
-   
 		success: function(data){
 		return 1;
 		}
@@ -23,7 +21,6 @@ function getUserInfo(userName) {
 			
 			$("#routineGreeting").html("Hi "+data[0].info.userName+" , select a workout routine to begin:");
 			for (var i =0; i< data[0].routines.length; i++) {
-				console.log(JSON.stringify(data[0].routines[i].name));
 				if (i % 3 == 0) {
 					$("#routineGreeting").append("<div class='row'>");
 				}
@@ -33,56 +30,62 @@ function getUserInfo(userName) {
 				$("#routineGreeting").append("<button type='button' class='btn btn-default'>"+data[0].routines[i].name+"</button>");
 			
 			}
-		console.log(JSON.stringify(data[0]._id));
 		CURRENT_USER_ID = data[0]._id;
 		return data[0];
 		}
 	})
 }
 
-function addRoutine( userID, newRoutineName) {
+function addRoutine( userID, newRoutineName, exArr) {
+	var dataObj = {};
+	dataObj['newRoutine'] = newRoutineName;
+
+	for (var i=0; i<exArr.length; i++){
+		dataObj[i] = exArr[i];
+	};
+
 	$.ajax({
 		type: 'PUT',
 		dataType: 'HTTP',
 		url:'../addroutine/'+userID,
-		data: {newRoutine: newRoutineName},
+		data: dataObj,
 		success: function(data) {
-			console.log('routine added');
-			console.log(newRoutineName);
+			return 1;
 		}
 	})	
 }	
 
 
 $('#createUser').click(function() {
- userName = $("#userName").val();
- firstName = $("#fname").val();
-lastName=  $("#lname").val();
-createUser(userName ,firstName,lastName );
-$("#createUserBucket").css("display","none");
-$("#userRoutines").css("display","block");
+ 	userName = $("#userName").val();
+	firstName = $("#fname").val();
+	lastName=  $("#lname").val();
+	createUser(userName ,firstName,lastName );
+	$("#createUserBucket").css("display","none");
+	$("#userRoutines").css("display","block");
 });
 
+
 $('#createUserForm').click(function() {
- $("#loginBucket").css("display","none");
-$("#createUserBucket").css("display","block");
+ 	$("#loginBucket").css("display","none");
+	$("#createUserBucket").css("display","block");
+	CURRENT_USER= userName ;
 });
 
 
 
 $("#logIn").click(function() {
-	 $("#loginBucket").css("display","none");
+	$("#loginBucket").css("display","none");
 	$("#userRoutines").css("display","block");
 	newUser = $('#logInField').val();
 	getUserInfo(newUser);
 	CURRENT_USER = newUser;
-
-
 });
 
-$('#createRoutine').click(function() {
- $("#userRoutines").css("display","none");
-$("#createRoutineBucket").css("display","block");
+
+$('#createRoutine').click(function() { 	
+	$("#userRoutines").css("display","none");
+	$("#createRoutineBucket").css("display","block");
 	
 });
 
@@ -90,13 +93,20 @@ $("#createRoutineBucket").css("display","block");
 
 $('#saveRoutine').click(function() {
 	var newRoutineName = $('#newRoutineName').val().toString();
-	console.log(CURRENT_USER_ID);
-	console.log(newRoutineName);
-	addRoutine(CURRENT_USER_ID, newRoutineName);
+	var exArr = [];
+	var elements = document.getElementsByClassName('inputCreateRoutineName');
+	console.log(elements[1].value.toString());
+	for (var i=0; i< elements.length; i++){
+		if (elements[i].value !== ""){
+		
+			exArr.push(elements[i].value);
+		}
+	}
+
+	addRoutine(CURRENT_USER_ID, newRoutineName, exArr);
 	$("#userRoutines").css("display","block");
 	$("#createRoutineBucket").css("display","none");
 	getUserInfo(newUser);
-	
 });
 
 
--- routes/addRoutine.js
+++ routes/addRoutine.js
@@ -3,10 +3,18 @@ var router = express.Router();
 var wuUser = require('../models/wuUsers.js');
 
 router.route('/:user_id').put(function(req, res) {
+	var keys = Object.keys(req.body);
+	var exArr =[];
+
+	for (var i=0; i<keys.length; i++){
+		if (keys[i] !== 'newRoutine') {
+			exArr.push(req.body[keys[i]]);
+		};
+	};
 	
 	wuUser.findByIdAndUpdate( 
 		req.params.user_id,
-		{$push : {"routines" : { name : req.body.newRoutine}}},
+		{$push : {"routines" : { name : req.body.newRoutine, routineExercises: exArr}}},
 		function(err, data) {
 			if (err) {
 				res.json({message : 'Error, could not add routine :('});
